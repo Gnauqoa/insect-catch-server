4f1799ee45550dc7295bda215a5cf615daa11ad7
Gnauqoa <quanglng2022@gmail.com>
2023-04-09 17:39:43 +0700

    add services/formatDeviceRes

---
+++ src/services/formatDeviceRes.js
@@ -0,0 +1,20 @@
+const formatDeviceRes = (device) => {
+  return {
+    id: device._id,
+    battery: device.battery,
+    brightness: device.brightness,
+    coordinates: device.coordinates,
+    humi: device.humi,
+    location: device.location,
+    optic: device.optic,
+    temp: device.temp,
+    rain: device.rain,
+    status: device.status,
+    grid_status: device.grid_status,
+    time_end: device.time_end,
+    time_start: device.time_start,
+    time_send: device.time_send,
+  };
+};
+
+export default formatDeviceRes;
