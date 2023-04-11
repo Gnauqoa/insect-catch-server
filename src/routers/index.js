e1f129023d2e985780e5a6dd3d75ea2ba4a5553e
Gnauqoa <quanglng2022@gmail.com>
2023-04-11 16:21:54 +0700

    Add wakeUp request

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,15 +6,5 @@ const router = Router();
 
 router.use("/user", userRouter);
 router.use("/device", deviceRouter);
-router.get(
-  "/",
-  (req, res) =>
-    async function (req, res) {
-      try {
-        res.status(200).json({ message: "OK" });
-      } catch (err) {
-        res.status(500).json({ message: err.message });
-      }
-    }
-);
+
 export default router;
