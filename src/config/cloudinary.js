a9da4194d39ffa655c0e6f9d962de4f6e69b126d
Gnauqoa <quanglng2022@gmail.com>
2023-04-23 14:31:20 +0700

    test

---
+++ src/config/cloudinary.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from "cloudinary";
+import {} from "dotenv/config";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  allowedFormats: ["jpg", "png"],
+  filename: function (req, file, cb) {
+    console.log("Here");
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+export default upload;
--- src/routers/user.js
+++ src/routers/user.js
@@ -11,6 +11,7 @@ import { Router } from "express";
 import userAuth from "../middleware/userAuth.js";
 import addedAuth from "../middleware/addedAuth.js";
 import { getDevice, updateDevice } from "../controller/device/index.js";
+import upload from "../config/cloudinary.js";
 
 const userRouter = Router();
 
@@ -22,6 +23,14 @@ userRouter.get("/current", userAuth, getUser);
 userRouter.delete("/current/logout", userAuth, logout);
 userRouter.delete("/current/logout_all", userAuth, logoutAll);
 
+userRouter.post("/test", upload.single("image"), async (req, res) => {
+  try {
+    res.status(200).json({ file: req.file });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to upload image" });
+  }
+});
 userRouter.post("/current/add_device", userAuth, addDevice);
 userRouter.get("/current/device/:device_id", userAuth, addedAuth, getDevice);
 userRouter.put("/current/device/:device_id", userAuth, addedAuth, updateDevice);
