b897d802dde280a5ec83526d48bb0cb4eec6aca0
Gnauqoa <quanglng2022@gmail.com>
2023-05-23 11:26:55 +0700

    config/express and config/mongoose

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
 import express from "express";
-import cors from "cors";
 import {} from "dotenv/config";
 import mongoose from "mongoose";
-import router from "./src/routers/index.js";
+import configExpressApp from "./src/config/express.js";
+import configMongoose from "./src/config/mongoose.js";
 
-const port = process.env.PORT || 4000;
-const database = process.env.DATABASE_URL;
 const app = express();
-const version = "/v2";
 
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-mongoose.set("strictQuery", false); // hide notify in console
+configExpressApp(app);
+configMongoose(mongoose);
 
-app.use(version, router);
-
-app.use((error, req, res, next) => {
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message: message, data: data });
+app.listen(app.get("port"), () => {
+  console.log(`start server at port: ${app.get("port")}`);
 });
-app.listen(port, () => {
-  console.log(`start server at port: ${port}`);
-  mongoose
-    .connect(database)
-    .then((result) => {
-      console.log(`connect database with url: ${database}`);
-    })
-    .catch((err) => console.log(err));
-});
-app.get(
-  "/",
-  async function (req, res) {
-    try {
-      res.status(200).json({ message: "OK" });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  }
-);
